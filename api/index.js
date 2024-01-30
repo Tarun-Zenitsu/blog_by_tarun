3de0dd4f69036b09a3cd0f90529f4e9ba608689b
Tarunakanta Kumbhar <tarunakantakumbhar@Tarunakantas-MacBook-Air.local>
2024-01-30 16:46:30 +0530

    connect to the database

--- api/index.js
+++ api/index.js
@@ -2,6 +2,20 @@ import express from 'express';
 
 const app = express();
 
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGOOSE_URL)
+  .then(() => {
+    console.log('MongoDb is connected');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
 
 app.listen(3000, () => {
     console.log('app is listen on port number: 3000');

a31eb99ea184bfc9109f8a4a608dd9c7b8c962a5
Tarunakanta Kumbhar <tarunakantakumbhar@Tarunakantas-MacBook-Air.local>
2024-01-30 16:07:49 +0530

    create a server for backend

---
+++ api/index.js
@@ -0,0 +1,8 @@
+import express from 'express';
+
+const app = express();
+
+
+app.listen(3000, () => {
+    console.log('app is listen on port number: 3000');
+})
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,27 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from "./pages/Home";
+import About from './pages/About';
+import SignUp from './pages/SignUp'
+import SignIn from './pages/SignIn';
+import Dashboard from './pages/Dashbord';
+import Projects from './pages/Projects'
+import Header from './components/Header';
+
+
 function App() {
 
   return (
-    <div className='bg-red-500'>hi there</div>
+    <BrowserRouter>
+    <Header />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/sign-in" element={<SignIn />} />
+      <Route path="/sign-up" element={<SignUp />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/projects" element={<Projects />} />
+    </Routes>
+  </BrowserRouter>
   )
 }
 

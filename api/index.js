acf594372d90155a69e54f4917ca916d56e83f99
Tarunakanta kumbhar <btkkumbhar1998@gmail.com>
2024-01-31 12:01:27 +0530

    add a middleware and a function to handel error

--- api/index.js
+++ api/index.js
@@ -26,4 +26,15 @@ app.listen(3000, () => {
 })
 
 app.use('/api/user', userRoutes);
-app.use('/api/auth', authRoutes)
+app.use('/api/auth', authRoutes);
+
+app.use((error, req, res, next) => {
+  const statusCode = error.statusCode || 500 ;
+  const message = error.message || "Internal Server Error";
+  
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message
+  })
+})

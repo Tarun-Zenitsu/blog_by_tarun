083cbd5a3983f10391d1866d2b225cff6012bd4b
Tarunakanta kumbhar <btkkumbhar1998@gmail.com>
2024-02-02 12:46:01 +0530

    create and add signin api

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from '../uitils/error.js';
+import jwt from 'jsonwebtoken'
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -31,3 +32,35 @@ export const signup = async (req, res, next) => {
     next(error);
   }
 };
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (
+    !email ||
+    !password ||
+    email === '' ||
+    password === ''
+  ) {
+    next(errorHandler(401, "All fields are required"))
+  }
+  try {
+    const validUser = await User.findOne({ email })
+    if(!validUser) {
+      return next(errorHandler(400, 'User not found'))
+    }
+    const validPassword = await bcryptjs.compare(password, validUser.password)
+    const { password: pass, ...rest } = validUser._doc; 
+    if(!validPassword){
+      return next(errorHandler(400, 'Incorect password'))
+    }
+    const token = jwt.sign(
+      {id: validUser._id}, process.env.JWT_SECRET
+    )
+    res.status(200).cookie('access_token', token, {
+      httpOnly: true
+    }).json(rest);
+  } catch (error) {
+    next(error)
+  }
+}

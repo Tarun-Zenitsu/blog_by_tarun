5ce754022c4b0c59b38eec0caaad55d9348912ac
Tarunakanta kumbhar <btkkumbhar1998@gmail.com>
2024-02-12 13:13:32 +0530

    add update user api route

--- api/index.js
+++ api/index.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import userRoutes from './routes/user.route.js'
 import authRoutes from './routes/auth.route.js'
+import cookieParser from 'cookie-parser'
 
 const app = express();
 
 app.use(express.json())
+app.use(cookieParser())
 
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';

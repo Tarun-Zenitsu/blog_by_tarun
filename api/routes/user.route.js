874eeb9ff844653395c2aab1985bc20eefa8238a
Tarunakanta kumbhar <btkkumbhar1998@gmail.com>
2024-02-12 17:43:59 +0530

    complete update user profile page functionality

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { test, updateUser } from '../controllers/user.cntroller.js';
-import { verifyToken } from '../uitils/verifyUser.js';
+import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
 

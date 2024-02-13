a8589312114b6e3321287ce5ad608594ef2beaa3
Tarunakanta kumbhar <btkkumbhar1998@gmail.com>
2024-02-13 12:06:39 +0530

    add admin functionality to user

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -55,7 +55,7 @@ export const signin = async (req, res, next) => {
       return next(errorHandler(400, 'Incorect password'))
     }
     const token = jwt.sign(
-      {id: validUser._id}, process.env.JWT_SECRET
+      {id: validUser._id, isAdmin: validUser.isAdmin}, process.env.JWT_SECRET
     )
     res.status(200).cookie('access_token', token, {
       httpOnly: true
@@ -70,7 +70,7 @@ export const google = async (req, res, next) => {
   try {
     const user = await User.findOne({ email });
     if (user) {
-      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+      const token = jwt.sign({ id: user._id, isAdmin: user.isAdmin }, process.env.JWT_SECRET);
       const { password, ...rest } = user._doc;
       res
         .status(200)
@@ -92,7 +92,7 @@ export const google = async (req, res, next) => {
         profilePicture: googlePhotoUrl,
       });
       await newUser.save();
-      const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
+      const token = jwt.sign({ id: newUser._id, isAdmin: newUser.isAdmin }, process.env.JWT_SECRET);
       const { password, ...rest } = newUser._doc;
       res
         .status(200)
